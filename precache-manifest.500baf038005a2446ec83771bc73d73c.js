a2b718bc4013ae32d4626eb59e69dd915b30fe27
georgewbrown <396gbrown@gmail.com>
2020-01-24 17:01:02 -0500

    Updates

---
+++ precache-manifest.500baf038005a2446ec83771bc73d73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8fb3aae1d46d29bf2478912e246ab1",
+    "url": "/game-grader/index.html"
+  },
+  {
+    "revision": "fa6b25750a8175a95ee9",
+    "url": "/game-grader/static/css/main.742fd12e.chunk.css"
+  },
+  {
+    "revision": "c23d7161c3adad97f046",
+    "url": "/game-grader/static/js/2.2406ea03.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/game-grader/static/js/2.2406ea03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa6b25750a8175a95ee9",
+    "url": "/game-grader/static/js/main.93f8bedf.chunk.js"
+  },
+  {
+    "revision": "9bdb52d26289fbc24f17",
+    "url": "/game-grader/static/js/runtime-main.f3654d5b.js"
+  }
+]);
